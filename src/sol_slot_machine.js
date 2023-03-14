1212586d250acb8acdb6d11355905655558cc362
yday05506 <yangdayeon2@gmail.com>
2023-03-15 08:48:07 +0900

    슬롯머신 문제

--- src/sol_slot_machine.js
+++ src/sol_slot_machine.js
@@ -2,17 +2,16 @@ import React from "react"
 import ReactDOM from 'react-dom'
 
 function SlotMachine({s1, s2, s3}) {
+    if((s1 === '7') && (s2 === '7') && (s3 === '7')) return <p>{s1} {s2} {s3} <br /> <div style={{color:'red'}}>Congrats!</div></p>
     if((s1 === s2) && (s2 === s3)) return <p>{s1} {s2} {s3} <br />Congrats!</p>
-    if((s1 === '7') && (s2 === '7') && (s3 === '7')) return <p style={{color : 'red'}}>Congrats!</p>
+    else return <p>{s1} {s2} {s3}</p>
 }
 
-<>
+ReactDOM.render(
+<div>
 <SlotMachine s1="X" s2="Y" s3="Z" />
 <SlotMachine s1="X" s2="X" s3="X" />
 <SlotMachine s1="7" s2="7" s3="7" />
-// 과일 이모지 : 🍇🍈🍉🍊🍌🍍🍎🍏🍑🍒🍓🍅🍆🌽🍄🌰
 <SlotMachine s1="🍓" s2="🍒" s3="🍍" />
 <SlotMachine s1="🍒" s2="🍒" s3="🍒" />
-</>
-
-ReactDOM.render(<SlotMachine />, document.getElementById("root"));
+</div>, document.getElementById("root"));
