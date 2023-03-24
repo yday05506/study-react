c10e2745d49f676ae9426d855f34abb0ee495db8
yday05506 <yangdayeon2@gmail.com>
2023-03-24 10:16:07 +0900

    카트

--- src/market.js
+++ src/market.js
@@ -1,79 +1,45 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { v4 } from 'uuid'
 
-const cartItem = function({cart: {text}, idx, handleCartStatusToggle, handleCartRemove}) {
-    return (
-        <div>
-            <button onClick={() => handleCartRemove(idx)}>X</button>
-        </div>
-    )
-}
-
-const CartList = function({items, handleCartStatusToggle, handleCartRemove}) {
-    return (
-        <ol>
-            {
-                items.map((cart, idx) => {
-                    return (
-                        <li key={idx}>
-                            <cartItem idx = {idx} cart={cart}
-                                handleCartStatusToggle = {handleCartStatusToggle}
-                                handleCartRemove = {handleCartRemove}/>
-                        </li>
-                    )
-                })
-            }
-        </ol>
-    )
-}
-
-const CartAdder = function({handleCartAdd}) {
-    const [input, setInput] = useState("")
-    const handleChange = (e) => setInput(e.target.value)
-    return (
-        <div>
-            <input type='text' onChange={handleChange} value={input} />
-            <button onClick={() => {
-                handleCartAdd({text: input})
-                setInput("")
-            }}>+</button>
-        </div>
-    )
-}
-
-const CartApp = function(props) {
-    const [carts, setCarts] = useState([
-        {text:'사과'},
-        {text:'바나나'}
+function CartApp(props) {
+    const [itemList, setItemList] = useState([
+        {id: v4(), name: "라면", count: "5"},
+        {id: v4(), name: "생수(500ml)", count: "2"}
     ])
-
-    const handleCartAdd = newCart => setCarts(items => items.concat(newCart))
-    const handleCartStatusToggle = cartIndex => {
-        setCarts(items => {
-            return items.map((cart, idx) => {
-                if(idx === cartIndex) {
-                    return {
-                        ...cart
-                    }
-                }
-                return cart
-            })
-        })
-    }
-    const handleCartRemove = cartIndex => {
-        setCarts(items => {
-            return items.filter((idx) => {
-                return idx !== cartIndex
-            })
-        })
-    }
+    const addListItem = (name) => setItemList(list => list.concat({id: v4(), name, count: 1}))
 
     return (
         <div>
-            <CartList items={carts}
-                handleCartStatusToggle = {handleCartStatusToggle}
-                handleCartRemove = {handleCartRemove} />
-            <CartAdder handleCartAdd = {handleCartAdd} />
+            <Input onButtonClick={addListItem} inputPlaceHolder="추가할 항목을 입력하세요." buttonLabel="검색" buttonColor="green"/> 
+            {
+                itemList.map(item => <CartItem item={item} />)
+                // itemList.map(item => <div>{item.name} 수량 : {item.count}</div>)
+            }   
+        </div>
+    )
+}
+
+function Input(props) {
+    const [text, setText] = useState("")
+    const handleOnChange = (e) => {setText(e.target.value)}
+
+    return (
+        <div>
+            <input type="text" placeHolder={props.inputPlaceHolder} onChange={handleOnChange} value={text}/> 
+            <button onClick={() => {
+                props.onButtonClick(text) 
+                setText("")
+                }} style={{background:props.buttonColor}}>{props.buttonLabel}</button>
+        </div>
+    )
+}
+
+function CartItem({ item: { name, count }} /*props */) {
+    return (
+        <div>
+            {/* {props.item.name} 수량 : {props.item.count} */}
+            {name} 수량 : {count}
         </div>
     )
 }
