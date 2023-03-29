53a64ce4550d414da0242b04db3818dab0df39f8
yday05506 <yangdayeon2@gmail.com>
2023-03-29 10:15:08 +0900

    마켓(완성)

--- src/market.js
+++ src/market.js
@@ -8,12 +8,28 @@ function CartApp(props) {
         {id: v4(), name: "생수(500ml)", count: "2"}
     ])
     const addListItem = (name) => setItemList(list => list.concat({id: v4(), name, count: 1}))
+    const addItemCount = (id, amount) => setItemList(list => {
+        return list.map(item => {
+            if(item.id === id) {
+                item = {...item, count: item.count + amount}
+            }
+            return item
+        })
+    })
+    const deleteListItem = (id) => setItemList(list => {
+        return list.filter(item => {
+            return item.id !== id
+        })
+    })
 
     return (
         <div>
             <Input onButtonClick={addListItem} inputPlaceHolder="추가할 항목을 입력하세요." buttonLabel="검색" buttonColor="green"/> 
             {
-                itemList.map(item => <CartItem item={item} />)
+                itemList.map(item => <CartItem 
+                    handleCountChange={addItemCount} 
+                    handleItemDelete={deleteListItem} 
+                    item={item} />)
                 // itemList.map(item => <div>{item.name} 수량 : {item.count}</div>)
             }   
         </div>
@@ -35,11 +51,21 @@ function Input(props) {
     )
 }
 
-function CartItem({ item: { name, count }} /*props */) {
+{/* props 대신 { item: { name, count } } 를 써도 됨*/ }
+function CartItem(props) {
     return (
         <div>
-            {/* {props.item.name} 수량 : {props.item.count} */}
-            {name} 수량 : {count}
+            {props.item.name} 수량 : {props.item.count}
+            {/* {name} 수량 : {count}/ */}
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={() => props.handleCountChange(props.item.id, 1)}>+</button>
+            &nbsp;
+            <button onClick={() => {
+                if(props.item.count === 1) props.handleItemDelete(props.item.id)
+                else props.handleCountChange(props.item.id, -1)}}>-</button>
+            &nbsp;
+            <button onClick={() => props.handleItemDelete(props.item
+                .id)}>x</button>
         </div>
     )
 }
